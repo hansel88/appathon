3e317a101ce0d420ed374fdd32d87ecd0499d6a7
Kristofer Selbekk <kris@kristoferselbekk.com>
2012-09-14 21:01:18 +0200

    StorageController ferdig (?)

---
+++ ScrumApp/SharedResources/Controller/StorageController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedResources.Model;
+using SharedResources.Utilities;
+using Windows.Foundation;
+using Windows.Storage;
+
+namespace SharedResources.Controller
+{
+    public static class StorageController
+    {
+        /* 
+         * This class handles all saving and loading of information
+         */
+
+        public static void LoadData()
+        {
+            // Fetching the settings
+            ApplicationDataContainer roamingSettings = ApplicationData.Current.RoamingSettings;
+
+            // Saving the settings temporarily in the app
+            DataStructure.Projects = roamingSettings.Values["Projects"] as List<Project>;
+        }
+
+        public static void SaveData()
+        { 
+            // Fetching the settings
+            ApplicationDataContainer roamingSettings = ApplicationData.Current.RoamingSettings;
+
+            // Saving the settings in the cloud
+            roamingSettings.Values["Projects"] = DataStructure.Projects;
+        }
+
+        public void InitHandlers()
+        {
+            ApplicationData.Current.DataChanged +=
+               new TypedEventHandler<ApplicationData, object>(DataChangeHandler);
+        }
+
+        private void DataChangeHandler(ApplicationData data, object o)
+        { 
+            // Refresh data here!
+            LoadData();
+        }
+
+
+    }
+}
---
+++ ScrumApp/SharedResources/Utilities/DataStructure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharedResources.Model;
+
+namespace SharedResources.Utilities
+{
+    public static class DataStructure
+    {
+        public static List<Project> Projects { get; set; }
+
+        // Methods that deals with finding and / or editing these projects
+    }
+}
