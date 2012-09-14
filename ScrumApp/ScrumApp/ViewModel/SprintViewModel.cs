f66f784e030081e1c7ad1ebc4d48f7ed089210ce
Hans-Petter Naumann <hpn_x@hotmail.com>
2012-09-14 22:15:07 +0200

    sprintview in progress

--- ScrumApp/ScrumApp/ViewModel/SprintViewModel.cs
+++ ScrumApp/ScrumApp/ViewModel/SprintViewModel.cs
@@ -24,9 +24,79 @@ namespace ScrumApp.ViewModel
             }
         }
 
+        private List<Story> _open;
+        public List<Story> Open
+        {
+            get
+            {
+                return _open;
+            }
+            set
+            {
+                _open = value;
+                onPropertyChanged("Open");
+            }
+        }
+
+        private List<Story> _inAnalysis;
+        public List<Story> InAnalysis
+        {
+            get
+            {
+                return _inAnalysis;
+            }
+            set
+            {
+                _inAnalysis = value;
+                onPropertyChanged("InAnalysis");
+            }
+        }
+
+        private List<Story> _inProgress;
+        public List<Story> InProgress
+        {
+            get
+            {
+                return _inProgress;
+            }
+            set
+            {
+                _inProgress = value;
+                onPropertyChanged("InProgress");
+            }
+        }
+
+        private List<Story> _testing;
+        public List<Story> Testing
+        {
+            get
+            {
+                return _testing;
+            }
+            set
+            {
+                _testing = value;
+                onPropertyChanged("Testing");
+            }
+        }
+
+        private List<Story> _closed;
+        public List<Story> Closed
+        {
+            get
+            {
+                return _closed;
+            }
+            set
+            {
+                _closed = value;
+                onPropertyChanged("Closed");
+            }
+        }
+
         public SprintViewModel()
         {
-
+            
         }
     }
 }
