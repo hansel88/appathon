ce4759add54f2a5493243023f5e13d74f22a979e
Hans-Petter Naumann <hpn_x@hotmail.com>
2012-09-14 22:08:30 +0200

    lol

---
+++ ScrumApp/ScrumApp/ViewModel/SprintViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrumApp.ViewModel;
+using SharedResources.Model;
+
+namespace ScrumApp.ViewModel
+{
+    class SprintViewModel : ViewModelBase
+    {
+        private Sprint _currentSprint;
+        public Sprint CurrentSprint
+        {
+            get
+            {
+                return _currentSprint;
+            }
+            set
+            {
+                _currentSprint = value;
+                onPropertyChanged("CurrentSprint");
+            }
+        }
+
+        public SprintViewModel()
+        {
+
+        }
+    }
+}
