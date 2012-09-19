afc55cbcad81101d4c322566bc3ec689c8d4111f
Hans-Petter Naumann <hpn_x@hotmail.com>
2012-09-19 20:21:28 +0200

    adduserview almost there

--- ScrumApp/ScrumApp/View/AddUserView.xaml.cs
+++ ScrumApp/ScrumApp/View/AddUserView.xaml.cs
@@ -11,6 +11,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using ScrumApp.ViewModel;
+using System.Text.RegularExpressions;
 
 // The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
 
@@ -21,9 +23,12 @@ namespace ScrumApp.View
     /// </summary>
     public sealed partial class AddUserView : ScrumApp.Common.LayoutAwarePage
     {
+        private AddUserViewModel addUserViewModel;
+
         public AddUserView()
         {
             this.InitializeComponent();
+            addUserViewModel = new AddUserViewModel();
         }
 
         /// <summary>
@@ -51,13 +56,75 @@ namespace ScrumApp.View
 
         private void SaveAndAddAnotherBtn_Click_1(object sender, RoutedEventArgs e)
         {
-
+            if (saveUser())
+            {
+                // clear fields, everything went OK
+                clearErrorText();
+                clearFields();
+            }
         }
 
         private void SaveAndExitBtn_Click_1(object sender, RoutedEventArgs e)
         {
+            if (saveUser())
+            {
+                this.Frame.GoBack();
+            }
+        }
+
+        private bool saveUser()
+        {
+            bool ok = true;
+
+            if (txtRealName.Text.Length == 0 || txtUserName.Text.Length == 0 || txtEmail.Text.Length == 0 || txtPhone.Text.Length == 0 || PermissionBox.SelectedItem == null)
+            {
+                errorBox.Text = "*Name, Username, email and permission level must be filled in.\n";
+                errorBox.Visibility = Visibility.Visible;
+                ok = false;
+            }
+            if (txtPassword.Password.Length < 5)
+            {
+                errorBox.Text += "*The password must atleast 5 characters long.\n";
+                ok = false;
+            }
+            if (!IsValidEmailAddress(txtEmail.Text))
+            {
+                errorBox.Text += "*Invalid email-address.\n";
+                ok = false;
+            }
+            // TOOO: Check if username already exists
+            if (ok)
+            {
+                addUserViewModel.saveUser(txtRealName.Text, txtUserName.Text, txtPassword.Password, txtEmail.Text, txtPhone.Text, PermissionBox.SelectedItem as string);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
         }
 
+        private bool IsValidEmailAddress(string email)
+        {
+            var regex = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+            return regex.IsMatch(email);
+        }
+
+        private void clearErrorText()
+        {
+            errorBox.Text = "";
+            errorBox.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void clearFields()
+        {
+            txtRealName.Text = "";
+            txtUserName.Text = "";
+            txtPassword.Password = "";
+            txtEmail.Text = "";
+            txtPhone.Text = "";
+        }
     }
 }
--- ScrumApp/ScrumApp/ViewModel/AddUserViewModel.cs
+++ ScrumApp/ScrumApp/ViewModel/AddUserViewModel.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharedResources.Utilities;
+using SharedResources.Model;
 
 namespace ScrumApp.ViewModel
 {
     class AddUserViewModel : ViewModelBase
     {
-        public void saveUser()
+        public void saveUser(string name, string userName, string password, string email, string phone, string permissionLevelString)
         {
+            PermissionLevel permissionLevel = new PermissionLevel();
+            switch (permissionLevelString)
+            {
+                case "User": permissionLevel = PermissionLevel.UserOnly;
+                    break;
+                case "Administrator": permissionLevel = PermissionLevel.AdminOnly;
+                    break;
+                case "Open": permissionLevel = PermissionLevel.Open;
+                    break;
+            }
 
+            User newUser = new User();
+
+            newUser.RealName = name;
+            newUser.UserName = userName;
+            newUser.Password = password;
+            newUser.Email = email;
+            newUser.PhoneNumber = phone;
+            newUser.AccessLevel = permissionLevel;
+
+            DataStructure.Users.Add(newUser);
         }
     }
 }
