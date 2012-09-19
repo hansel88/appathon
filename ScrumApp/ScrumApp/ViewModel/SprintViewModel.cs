34e4fdfdc9cc1cf074cb4f1f8abd50cf0a94dd91
Hans-Petter Naumann <hpn_x@hotmail.com>
2012-09-19 13:30:01 +0200

    .

--- ScrumApp/ScrumApp/ViewModel/SprintViewModel.cs
+++ ScrumApp/ScrumApp/ViewModel/SprintViewModel.cs
@@ -164,7 +164,7 @@ namespace ScrumApp.ViewModel
             tempUser2.RealName = "Kris Selbekk";
             UserStory2.Author = tempUser;
             UserStory2.CreatedDate = DateTime.Now;
-            UserStory.Priority = 900;
+            UserStory2.Priority = 900;
 
             Comment c12 = new Comment();
             c12.Author = tempUser;
